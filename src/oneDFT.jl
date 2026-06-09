7dcc47431cf0d8254a22b95406e46ef7c057d50b
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2026-06-09 12:13:19 -0700

    Add files via upload

--- src/oneDFT.jl
+++ src/oneDFT.jl
@@ -1,1 +1,31 @@
+module oneDFT
 
+  """
+    libdir
+
+  Shows where the One-DFT is in the filesystem
+  """
+  const libdir = @__DIR__
+
+  const libpath = libdir*"/lib/"
+
+  files = ["imports.jl",#="banner.jl",=#"exports.jl"]
+  for w = 1:length(files)
+    include(libpath*files[w])
+  end
+
+  files = ["lda.jl","exchangecorrelation.jl"]
+  subdir = "lda/"
+  for w = 1:length(files)
+    include(libpath*subdir*files[w])
+  end
+
+  files = ["findvKS.jl"]
+  subdir = "exactKS/"
+  for w = 1:length(files)
+    include(libpath*subdir*files[w])
+  end
+
+end
+
+using .oneDFT
